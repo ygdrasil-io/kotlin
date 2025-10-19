337c6406a30f3dfaedaa5f7e5a75c7cf1a88cf36
ygdrasil-io <alexandre.mommers@gmail.com>
2025-10-19 13:31:59 +0200

    Add initial implementation of AltRuntime for extended C interop

---
+++ kotlin-native/Interop/AltRuntime/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.gradle.kotlin.dsl.project
+
+plugins {
+    kotlin("multiplatform")
+}
+
+
+kotlin {
+
+    jvm()
+
+    if (kotlinBuildProperties.isInIdeaSync) {
+        val hostOs = System.getProperty("os.name")
+        val isMingwX64 = hostOs.startsWith("Windows")
+        val nativeTarget = when {
+            hostOs == "Mac OS X" -> macosX64("native")
+            hostOs == "Linux" -> linuxX64("native")
+            isMingwX64 -> mingwX64("native")
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
+        nativeTarget.compilerOptions {
+            freeCompilerArgs.addAll(
+                    listOf(
+                            "-Xallow-kotlin-package",
+                            "-Xexpect-actual-classes",
+                            "-nostdlib",
+                    )
+            )
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":kotlin-stdlib"))
+            }
+        }
+    }
+}
